c4169d0dfe89a09aaad308d20de435ff3c677f23
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-06 16:45:27 -0600

    completed data for schema

--- DB/employee_schema.sql
+++ DB/employee_schema.sql
@@ -4,7 +4,6 @@ CREATE DATABASE employee_trackerDB;
 
 
 -- employee:
-
 -- id - INT PRIMARY KEY
 -- first_name - VARCHAR(30) to hold employee first name
 -- last_name - VARCHAR(30) to hold employee last name
@@ -12,24 +11,35 @@ CREATE DATABASE employee_trackerDB;
 -- manager_id - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
 
 CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
 
 );
 -- role:
-
 -- id - INT PRIMARY KEY
 -- title - VARCHAR(30) to hold role title
 -- salary - DECIMAL to hold role salary
 -- department_id - INT to hold reference to department role belongs to
 
 CREATE TABLE role (
+    role_id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INT,
+    PRIMARY KEY (role_id)
     
 );
 
 -- department:
-
 -- id - INT PRIMARY KEY
 -- name - VARCHAR(30) to hold department name
 
 CREATE TABLE department (
-    
+    department_id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (department_id)
 )
