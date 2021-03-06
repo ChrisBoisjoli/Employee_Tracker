42cdf0923e7978a2035ec4339ac05be033ca7c73
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-06 17:42:40 -0600

    added data to department seed

--- DB/department_seed.sql
+++ DB/department_seed.sql
@@ -3,3 +3,8 @@
 --     name VARCHAR(30),
 --     PRIMARY KEY (department_id)
 -- )
+
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Engineering");
+INSERT INTO department (name) VALUES ("Finance");
+INSERT INTO department (name) VALUES ("Legal");
