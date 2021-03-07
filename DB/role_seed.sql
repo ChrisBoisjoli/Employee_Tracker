ea52c9210a335e658510f9b4eee6871c73ea5098
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-06 18:00:22 -0600

    created role seed

--- DB/role_seed.sql
+++ DB/role_seed.sql
@@ -6,3 +6,12 @@
 --     PRIMARY KEY (role_id)
     
 -- );
+
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Lead", 100000.00, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 80000.00, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Lead Engineer", 150000.00, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 120000.00, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 125000.00, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Lawyer", 190000.00, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Legal Team Lead", 250000.00, 4);
+
