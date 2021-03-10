1c80a430d4c311e558d32498c2a90202484f7e17
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-10 15:02:50 -0600

    adding questions pertaining to this app

--- questions.js
+++ questions.js
@@ -23,71 +23,72 @@ const inquirer = require("inquirer");
     //update employee role
 const questions = [
     {
-        type: "input",
-        name: "name",
-        message: "What is your Employee's name?",
-        default: "Employee Name",
-        
-    },
-    {
         type: "list",
-        message: "What is your Employee's role?",
-        name: "role",
-        choices: ["Manager", "Engineer", "Intern"],
+        message: "What would you like to do?",
+        name: "choices",
+        choices: ["view all employees", "add employee", "add role", "add department","view all departments", "view all roles", "update employee role"],
         default: "ex: Manager",
     },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your Employee's ID?",
-        default: "0",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your Employee's email?",
-        validate:  function(email)
-        {if (/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email)){
-            return true;
-            }
-            else {
-            return "enter a valid email";
-            }
-        }
-    },
-    {
-        type: "input",
-        name: "github",
-        when: function (employee){
-            return employee.role === "Engineer";},
-        message: 'What is Engineer GitHub user name?',
-        default: "GitHub",   
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        when: function (employee){
-            return employee.role === "Manager";},
-        message: "What is your Manager's office number?",
-        default: "0",
+    // {
+    //     type: "input",
+    //     name: "name",
+    //     message: "What is your Employee's name?",
+    //     default: "Employee Name",
         
-    },
-    {
-        type: "input",
-        name: "school",
-        when: function (employee){
-            return employee.role === "Intern";},
-        message: "What is the Inter's school?",
-        default: "School",
+    // },
+   
+    // {
+    //     type: "input",
+    //     name: "id",
+    //     message: "What is your Employee's ID?",
+    //     default: "0",
+    // },
+    // {
+    //     type: "input",
+    //     name: "email",
+    //     message: "What is your Employee's email?",
+    //     validate:  function(email)
+    //     {if (/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email)){
+    //         return true;
+    //         }
+    //         else {
+    //         return "enter a valid email";
+    //         }
+    //     }
+    // },
+    // {
+    //     type: "input",
+    //     name: "github",
+    //     when: function (employee){
+    //         return employee.role === "Engineer";},
+    //     message: 'What is Engineer GitHub user name?',
+    //     default: "GitHub",   
+    // },
+    // {
+    //     type: "input",
+    //     name: "officeNumber",
+    //     when: function (employee){
+    //         return employee.role === "Manager";},
+    //     message: "What is your Manager's office number?",
+    //     default: "0",
+        
+    // },
+    // {
+    //     type: "input",
+    //     name: "school",
+    //     when: function (employee){
+    //         return employee.role === "Intern";},
+    //     message: "What is the Inter's school?",
+    //     default: "School",
 
-    },
-    {
-        type: "confirm",
-        name: "addAnother",
-        message:"Do you want to add another team member?",
-        default: true,
+    // },
+    // {
+    //     type: "confirm",
+    //     name: "addAnother",
+    //     message:"Do you want to add another team member?",
+    //     default: true,
         
-    },
+    // },
     
             ];
 module.exports = questions;
