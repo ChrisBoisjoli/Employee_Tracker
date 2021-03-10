195d849787bcda7f28444d7de6b0150e2f6b2630
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-10 15:20:23 -0600

    added questions to server.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const inquirer = require('inquirer');
 
 const questions = require("./questions");
+
+function init(){
+ inquirer.prompt(questions).then((response) => console.log(response))
+};
+init()
