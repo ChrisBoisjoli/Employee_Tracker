1a804192e415b7c99f42a8c696859fdf23fe083b
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-10 18:37:57 -0600

    connection start for view all employees

--- index.js
+++ index.js
@@ -16,23 +16,51 @@ var connection = mysql.createConnection(
     password: process.env.DB_PASSWORD,
 });
 
-connection.connect(function(err){
-    if (err) throw err;
-    console.log("connected to " + connection.threadId);
-    // showEmployee();
-    // showDepartment();
-    // showRole();
-    // showRoster();
-    connection.end();
-});
+// connection.connect(function(err){
+//     if (err) throw err;
+//     console.log("connected to " + connection.threadId);
+//     // showEmployee();
+//     // showDepartment();
+//     // showRole();
+//     // showRoster();
+//     connection.end();
+// });
 // 
-// const start = ()
+const start = () => {
+inquirer
+.prompt({
+    name: "choices",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["view all employees", "add employee", "add role", "add department","view all departments", "view all roles", "update employee role", "exit"]
+})
+.then((answer) => {
+  // based on their answer, either call the bid or the post functions
+  if (answer.choices === 'view all employees') {
+    showRoster();
+  } else {
+    connection.end();
+  }
+});
+};
 
 
 
+function showRoster(){
+    //Name then Role then department
+    let sql = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager_id  FROM employee INNER JOIN role ON role.role_id = employee.role_id INNER JOIN department ON role.department_id = department.department_id ORDER BY employee.id";
+    connection.query(sql, function(err, res){
+        if (err) throw err;
+        console.table(res);
+    });
+};
 
-
-
+connection.connect((err) => {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+  
 // function init(){
 //  inquirer.prompt(questions).then((response) => 
 //  {if (response.choices === 'view all roles')
